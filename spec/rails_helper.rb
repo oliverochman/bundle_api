b8463cf851f485893ac79f33a19fd04b966d2bf1
hmvitous <59612601+hmvitous@users.noreply.github.com>
2020-04-12 22:02:41 +0200

    Puts the coveralls require at the top

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,8 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
-
 require 'coveralls'
 Coveralls.wear_merged!('rails')
+
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'

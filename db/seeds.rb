f977272bad933f2eeeb4f240513ed5d735800f75
hmvitous <59612601+hmvitous@users.noreply.github.com>
2020-04-15 13:32:54 +0200

    Deletes the location from the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,11 @@
 events = Event.create(
   title: 'Play Soccer',
   description: 'We need more players',
-  location: 'Hagaparken',
   category: 'outdoors' 
 )
 
 events = Event.create(
   title: 'Walking on Sunshine',
   description: 'From Slussen to Skansen',
-  location: 'Slussen Tunnelbana Station',
   category: 'outdoors'
 )

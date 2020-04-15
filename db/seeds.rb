dde4b02035e790b760e76ebadfdb44306b342705
hmvitous <59612601+hmvitous@users.noreply.github.com>
2020-04-15 11:55:19 +0200

    Takes out extra parentheses

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@ events = Event.create(
   title: 'Play Soccer',
   description: 'We need more players',
   location: 'Hagaparken',
-  category: 'outdoors' )
+  category: 'outdoors' 
 )
 
 events = Event.create(
   title: 'Walking on Sunshine',
   description: 'From Slussen to Skansen',
   location: 'Slussen Tunnelbana Station',
-  category: 'outdoors')
+  category: 'outdoors'
 )
